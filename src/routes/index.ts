d91f3f03945c0ecd4602bc96c7de8427476f6dff
SCTech-MZ <silviocustodiotech@gmail.com>
2026-07-31 19:31:31 +0200

    Remove root and diagnostic routes
    Removed the root and diagnostic routes from the API.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,24 +10,6 @@ import pool  from "../database/postgre";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        mensagem: 'API Grow Games',
-        status: 'Online'
-    });
-});
-
-router.get('/diag', async (require, res) => {
-    try {
-        const { rows } = await pool.query(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name`);
-        const tabelas = rows.map(e => e.table_name);
-
-        res.json({ status: 'ok', tabelas_existentes: tabelas, total_tabelas: tabelas.length });
-    } catch (erro:any) {
-        res.status(500).json({erro:erro.message})
-    }
-})
-
 
 router.use('/auth', authRouter);
 router.use('/aparelhos', aparelhoRoute);
