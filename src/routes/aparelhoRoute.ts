f67b4716412e01d7bcb09f0be210123f85e3e4be
SCTech-MZ <silviocustodiotech@gmail.com>
2026-07-31 19:56:59 +0200

    Add TypeScript types to aparelhoRoute handlers

--- src/routes/aparelhoRoute.ts
+++ src/routes/aparelhoRoute.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router,Request,Response,NextFunction } from "express";
 import { AparelhoController } from "../controllers/AparelhoController";
 import { AuthMiddleware, superAdminOnly } from "../middlewares/AuthMiddleware";
 
@@ -6,19 +6,19 @@ const router = Router();
 
 const controller = new AparelhoController();
 
-router.get("/", AuthMiddleware, (req, res, next) =>
+router.get("/", AuthMiddleware, (req: Request, res: Response, next: NextFunction) =>
   controller.listar(req, res, next),
 );
-router.get("/:id", AuthMiddleware, (req, res, next) =>
+router.get("/:id", AuthMiddleware, (req: Request, res: Response, next: NextFunction) =>
   controller.BuscarPorId(req, res, next),
 );
-router.post("/", AuthMiddleware, superAdminOnly, (req, res, next) =>
+router.post("/", AuthMiddleware, superAdminOnly, (req: Request, res: Response, next: NextFunction) =>
   controller.create(req, res, next),
 );
 router.put("/:id", AuthMiddleware, (req, res, next) =>
   controller.update(req, res, next),
 );
-router.delete("/:id", AuthMiddleware, superAdminOnly, (req, res, next) =>
+router.delete("/:id", AuthMiddleware, superAdminOnly, (req: Request, res: Response, next: NextFunction) =>
   controller.remover(req, res, next),
 );
 
