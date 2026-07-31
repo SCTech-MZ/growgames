5eb1fe1056863170ebfaf52712215866dc2aa81d
SCTech-MZ <silviocustodiotech@gmail.com>
2026-07-31 14:25:34 +0200

    Delete src/repositories/routes directory

--- src/repositories/routes/authRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { AuthController } from "../../controllers/AuthController";
-
-const router = Router();
-const controller = new AuthController();
-
-router.post("/login", controller.login.bind(controller));
-
-export default router;
