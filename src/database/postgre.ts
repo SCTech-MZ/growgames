081bebe3033f634d700fffece90fdc208a81e046
SCTech-MZ <silviocustodiotech@gmail.com>
2026-08-03 12:32:17 +0200

    modificado package.json e conexao ao neon

--- src/database/postgre.ts
+++ src/database/postgre.ts
@@ -3,14 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+if(!process.env.DATABASE_URL){
+    throw new Error("DATABASE_URL nao definida no arquivo .env");
+}
+
 const pool = new Pool({
     connectionString:process.env.DABASE_URL,
     ssl:{ rejectUnauthorized:false},
-    max:5,
+    max: 5,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 10000,
 });
 
 pool.on('connect',()=>{
-    console.log("Conectado com sucesso");
+    console.log("Conectado com sucesso ao neon database");
 });
 pool.on('error',(erro:any)=>{
     console.log("Falha na conexao", erro.message);
